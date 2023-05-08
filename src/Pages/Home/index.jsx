228c1ddf8052ed3eadc52d52296ab2434790a579
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-05-07 23:19:07 -0600

    Finished card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react"
 import { Card } from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
 
+
 function Home() {
+  const [items, setitems] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(response => response.json())
+      .then( data => setitems(data))
+      
+  }, [])
+  
 
   return (
     <Layout>
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        { items?.map( item => ( 
+            <Card key={item.id} category={item.category.name} title={item.title} price={item.price} image={item.  images[0]}/>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
